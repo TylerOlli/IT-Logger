3f060697aee723114021bbb6b7963455c97a0ddc
TylerOlli <ollityler@gmail.com>
2019-10-15 19:47:42 -0400

    Connected Redux to Components

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -14,6 +14,7 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 //    };
 //  };
 
+// Get Logs From Server
 export const getLogs = () => async dispatch => {
   try {
     setLoading();
